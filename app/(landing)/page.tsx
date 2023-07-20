5e4ab6c288f5d54587428a1b0e20974f9e41e8c8
Anchit Sinha <anchit1909@gmail.com>
2023-07-21 00:30:52 +0530

    added sidebar component

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -12,7 +12,6 @@ const LandingPage = () => {
           <Button>Register</Button>
         </Link>
       </div>
-      
     </div>
   );
 };
---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sidebar } from "@/components/sidebar";
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
